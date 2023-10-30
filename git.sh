ea9b7526283b05bcd357a6b9c8397c39fe42f0d3
msaidcevik <m.s.cevik23@gmail.com>
2023-10-30 03:59:03 +0300

    added git.sh

---
+++ git.sh
@@ -0,0 +1,9 @@
+git --version
+git config --global credential.helper store
+git config --global user.name "user-name"
+git config --global user.email "email"
+ git add .
+ git commit -m "asssss"
+ git push
+    username
+    password 
